80224c43a37852b6df7dfbc1bf5362012164a3cf
TKirby42 <jimgorman2004@yahoo.com>
2014-02-23 11:35:16 -0500

    Delete an unused namespace.

--- ScoutingApp/src/main/java/com/jimg/scoutingapp/MainActivity.java
+++ ScoutingApp/src/main/java/com/jimg/scoutingapp/MainActivity.java
@@ -22,7 +22,6 @@ import android.view.MenuItem;
 import android.view.SubMenu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
