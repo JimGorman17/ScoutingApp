21ac6ee3820f598828ead70a946a1eb8ae02474e
TKirby42 <jimgorman2004@yahoo.com>
2014-04-06 10:24:24 -0400

    For the comment view list, use the ViewHolder pattern and implement click handlers for the buttons.

--- ScoutingApp/src/main/java/com/jimg/scoutingapp/utilityclasses/LazyAdapterForCommentViewPojo.java
+++ ScoutingApp/src/main/java/com/jimg/scoutingapp/utilityclasses/LazyAdapterForCommentViewPojo.java
@@ -7,10 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jimg.scoutingapp.R;
+import com.jimg.scoutingapp.helpers.DisplayToast;
 import com.jimg.scoutingapp.pojos.CommentViewPojo;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
@@ -22,11 +25,21 @@ import java.util.ArrayList;
  */
 public class LazyAdapterForCommentViewPojo extends BaseAdapter {
 
+    private static class ViewHolderItem {
+        ImageView userPictureImageView;
+        TextView userNameTextView;
+        TextView commentStringTextView;
+        ImageButton commentEditButton;
+        ImageButton commentDeleteButton;
+    }
+
+    private Activity mActivity;
     private ArrayList<CommentViewPojo> mCommentViewPojoList;
     private static LayoutInflater mInflater = null;
     private ImageLoader mImageLoader;
 
     public LazyAdapterForCommentViewPojo(Activity activity, ArrayList<CommentViewPojo> commentViewPojoList) {
+        mActivity = activity;
         mCommentViewPojoList = commentViewPojoList;
         mInflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         mImageLoader = ImageLoader.getInstance();
@@ -46,19 +59,43 @@ public class LazyAdapterForCommentViewPojo extends BaseAdapter {
 
     public View getView(int position, View convertView, ViewGroup parent) {
         View vi = convertView;
-        if (convertView == null) {
+        ViewHolderItem viewHolder;
+
+        if (vi == null) {
             vi = mInflater.inflate(R.layout.comment_list_row, null);
+
+            viewHolder = new ViewHolderItem();
+            viewHolder.userPictureImageView = (ImageView) vi.findViewById(R.id.columnUserPictureForComment);
+            viewHolder.userNameTextView = (TextView) vi.findViewById(R.id.columnUserDisplayNameForComment);
+            viewHolder.commentStringTextView = (TextView) vi.findViewById(R.id.columnCommentStringForPlayerPage);
+            viewHolder.commentEditButton = (ImageButton) vi.findViewById(R.id.comment_edit_button);
+            viewHolder.commentDeleteButton = (ImageButton) vi.findViewById(R.id.comment_delete_button);
+
+            viewHolder.commentEditButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    new DisplayToast(mActivity, "Edit button clicked.", Toast.LENGTH_LONG).run();
+                }
+            });
+
+            viewHolder.commentDeleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    new DisplayToast(mActivity, "Delete button clicked.", Toast.LENGTH_LONG).run();
+                }
+            });
+
+            vi.setTag(viewHolder);
+        }
+        else {
+            viewHolder = (ViewHolderItem) vi.getTag();
         }
 
         CommentViewPojo item = getItem(position);
 
-        ImageView userPictureImageView = (ImageView) vi.findViewById(R.id.columnUserPictureForComment);
-        TextView userNameTextView = (TextView) vi.findViewById(R.id.columnUserDisplayNameForComment);
-        TextView commentStringTextView = (TextView) vi.findViewById(R.id.columnCommentStringForPlayerPage);
-
-        mImageLoader.displayImage(item.PictureUrl, userPictureImageView);
-        userNameTextView.setText(item.DisplayName);
-        commentStringTextView.setText(Html.fromHtml(item.FormattedComment));
+        mImageLoader.displayImage(item.PictureUrl, viewHolder.userPictureImageView);
+        viewHolder.userNameTextView.setText(item.DisplayName);
+        viewHolder.commentStringTextView.setText(Html.fromHtml(item.FormattedComment));
 
         return vi;
     }
