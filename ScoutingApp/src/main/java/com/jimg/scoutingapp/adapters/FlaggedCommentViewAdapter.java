8897629165cb123f1ee63eb064e5126ce0ed70e5
TKirby42 <jimgorman2004@yahoo.com>
2014-04-13 18:24:23 -0400

    Added support for ignoring comment flags.

--- ScoutingApp/src/main/java/com/jimg/scoutingapp/adapters/FlaggedCommentViewAdapter.java
+++ ScoutingApp/src/main/java/com/jimg/scoutingapp/adapters/FlaggedCommentViewAdapter.java
@@ -1,15 +1,24 @@
 package com.jimg.scoutingapp.adapters;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.gson.JsonObject;
+import com.jimg.scoutingapp.Constants;
+import com.jimg.scoutingapp.MainActivity;
 import com.jimg.scoutingapp.R;
+import com.jimg.scoutingapp.helpers.ErrorHelpers;
 import com.jimg.scoutingapp.pojos.FlaggedCommentPojo;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
 
 import java.util.ArrayList;
 
@@ -22,26 +31,30 @@ import butterknife.InjectView;
 public class FlaggedCommentViewAdapter extends BaseAdapter {
 
     static class ViewHolderItem {
+        ListView parentListView;
         Integer commentId;
         String rawComment;
 
         @InjectView(R.id.columnFormattedCommentString) TextView formattedCommentTextView;
         @InjectView(R.id.columnNumberOfFlags) TextView flagsTextView;
+        @InjectView(R.id.comment_edit_button) ImageButton editButton;
+        @InjectView(R.id.comment_delete_button) ImageButton deleteButton;
+        @InjectView(R.id.comment_ignore_button) ImageButton ignoreButton;
 
-        private ViewHolderItem(View view) {
+        private ViewHolderItem(ListView parentListView, View view) {
+            this.parentListView = parentListView;
             ButterKnife.inject(this, view);
         }
     }
 
-    private Context mContext;
+    private MainActivity mMainActivity;
     private ArrayList<FlaggedCommentPojo> mFlaggedCommentPojoList;
     private static LayoutInflater mInflater = null;
-    public Integer mCurrentlySelectedCommentId = 0;
 
-    public FlaggedCommentViewAdapter(Context context, ArrayList<FlaggedCommentPojo> flaggedCommentPojoList) {
-        this.mContext = context;
+    public FlaggedCommentViewAdapter(MainActivity mainActivity, ArrayList<FlaggedCommentPojo> flaggedCommentPojoList) {
+        this.mMainActivity = mainActivity;
         this.mFlaggedCommentPojoList = flaggedCommentPojoList;
-        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mInflater = (LayoutInflater) mainActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
@@ -67,13 +80,54 @@ public class FlaggedCommentViewAdapter extends BaseAdapter {
         if (vi == null) {
             vi = mInflater.inflate(R.layout.flagged_comment_list_row, null);
 
-            viewHolder = new ViewHolderItem(vi);
+            viewHolder = new ViewHolderItem((ListView)parent, vi);
+            viewHolder.deleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+            viewHolder.ignoreButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    JsonObject json = new JsonObject();
+                    json.addProperty(Constants.commentIdExtra, viewHolder.commentId);
+                    json.addProperty(Constants.authTokenExtra, mMainActivity.mAuthToken);
+
+                    mMainActivity.mProgressDialog = ProgressDialog.show(mMainActivity, "", mMainActivity.getString(R.string.please_wait_ignoring_flags), false);
+                    Ion.with(mMainActivity, Constants.restServiceUrlBase + "FlaggedComment/IgnoreFlags?" + Constants.getJson)
+                            .progressDialog(mMainActivity.mProgressDialog)
+                            .setJsonObjectBody(json)
+                            .asJsonObject()
+                            .setCallback(new FutureCallback<JsonObject>() {
+                                @Override
+                                public void onCompleted(Exception e, JsonObject result) {
+                                    if (e != null) {
+                                        ErrorHelpers.handleError(mMainActivity.getString(R.string.failure_to_ignore_comments), e.getMessage(), ErrorHelpers.getStackTraceAsString(e), mMainActivity);
+                                    }
+                                    for (int i = 0; i < mFlaggedCommentPojoList.size(); i++) {
+                                        FlaggedCommentPojo flaggedComment = mFlaggedCommentPojoList.get(i);
+                                        if (flaggedComment.commentId == viewHolder.commentId) {
+                                            mFlaggedCommentPojoList.remove(flaggedComment);
+                                            break;
+                                        }
+                                    }
+                                    ((BaseAdapter) viewHolder.parentListView.getAdapter()).notifyDataSetChanged();
+
+                                    mMainActivity.dismissProgressDialog();
+                                }
+                            });
+                }
+            });
+
             vi.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolderItem) vi.getTag();
         }
 
         FlaggedCommentPojo item = getItem(position);
+        viewHolder.commentId = item.commentId;
         viewHolder.formattedCommentTextView.setText(Html.fromHtml(item.formattedComment));
         viewHolder.flagsTextView.setText(item.numberOfFlags.toString());
 
